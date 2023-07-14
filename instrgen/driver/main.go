915ba3d94598458ef3df2690fd839aeefa113b71
Przemek Delewski <pdelewski@users.noreply.github.com>
2023-07-14 13:45:12 +0200

    toolexec #3

--- instrgen/driver/main.go
+++ instrgen/driver/main.go
@@ -250,58 +250,58 @@ func GetCommandName(args []string) string {
 	return cmd
 }
 
-func inspectFuncs(file *ast.File, fset *token.FileSet, f *os.File) {
+func inspectFuncs(pkg string, file *ast.File, fset *token.FileSet, f *os.File) {
 	ast.Inspect(file, func(n ast.Node) bool {
 		if funDeclNode, ok := n.(*ast.FuncDecl); ok {
-			f.WriteString("FuncDecl:" + fset.Position(funDeclNode.Pos()).String() + file.Name.Name + "." + funDeclNode.Name.String())
+
+			f.WriteString("Package:" + pkg + " FuncDecl:" + fset.Position(funDeclNode.Pos()).String() + file.Name.Name + "." + funDeclNode.Name.String())
 			f.WriteString("\n")
 		}
 		return true
 	})
 }
 
-func analyzePackage(filePaths []string, f *os.File) {
+func analyzePackage(pkg string, filePaths map[string]int, f *os.File, destPath string, args []string) {
+	prog := `package main
+import "fmt"
+func main() {fmt.Println("hello2")}`
 	fset := token.NewFileSet()
-
-	var files []*ast.File
-	for _, filePath := range filePaths {
+	for filePath, index := range filePaths {
 		file, err := parser.ParseFile(fset, filePath, nil, 0)
 		if err != nil {
 			f.WriteString(err.Error())
 			f.WriteString("\n")
 		}
-		files = append(files, file)
-	}
-
-	for _, file := range files {
-		inspectFuncs(file, fset, f)
+		filename := filepath.Base(filePath)
+		if filename == "main.go" {
+			f.WriteString("create:" + destPath + "/" + "main.go")
+			f.WriteString("\n")
+			out, _ := os.Create(destPath + "/" + "main.go")
+			out.WriteString(prog)
+			out.Close()
+			args[index] = destPath + "/" + "main.go"
+		}
+		inspectFuncs(pkg, file, fset, f)
 	}
 }
 
-func compile(args []string) {
-	prog := `package main
-import "fmt"
-func main() {fmt.Println("hello")}`
+func toolExecMain(args []string) {
 	f, _ := os.OpenFile("args", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	argsLen := len(args)
 	var destPath string
+	var pkg string
 	for i, a := range args {
 		// output directory
 		if a == "-o" {
 			destPath = filepath.Dir(string(args[i+1]))
-			f.WriteString("dest path:" + destPath)
-			f.WriteString("\n")
 		}
 		// package
 		if a == "-p" {
-			pkg := string(args[i+1])
-			f.WriteString("package:" + pkg)
-			f.WriteString("\n")
+			pkg = string(args[i+1])
 		}
 		// source files
 		if a == "-pack" {
-			pathReported := false
-			var files []string
+			files := make(map[string]int)
 			for j := i + 1; j < argsLen; j++ {
 				// omit -asmhdr switch + following header+
 				if string(args[j]) == "-asmhdr" {
@@ -311,30 +311,12 @@ func main() {fmt.Println("hello")}`
 					continue
 				}
 				filePath := args[j]
-				filename := filepath.Base(filePath)
-				srcPath := filepath.Dir(filePath)
-				if !pathReported {
-					f.WriteString("src path:" + srcPath)
-					f.WriteString("\n")
-					pathReported = true
-				}
-				f.WriteString(filename)
-				f.WriteString("\n")
-				if filename == "main.go" {
-
-					out, _ := os.Create(destPath + "/" + "main.go")
-					out.WriteString(prog)
-					out.Close()
-					args[j] = destPath + "/" + "main.go"
-				}
-				files = append(files, filePath)
+				files[filePath] = j
 			}
-			analyzePackage(files, f)
-
+			analyzePackage(pkg, files, f, destPath, args)
 		}
 	}
 	executePass(args[0:])
-
 }
 
 func executeCommandProxy(cmdName string) {
@@ -366,6 +348,5 @@ func main() {
 		executePass(args[0:])
 		return
 	}
-	compile(args)
-
+	toolExecMain(args)
 }
