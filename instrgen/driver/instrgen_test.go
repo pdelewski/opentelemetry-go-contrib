4daf6cc4a551fdef2e94b2fec4b074c0171ebeb5
Przemek Delewski <pdelewski@users.noreply.github.com>
2023-07-21 09:34:29 +0200

    update tests

--- instrgen/driver/instrgen_test.go
+++ instrgen/driver/instrgen_test.go
@@ -17,30 +17,32 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	alib "go.opentelemetry.io/contrib/instrgen/lib"
 	"go.opentelemetry.io/contrib/instrgen/rewriters"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
 var testcases = map[string]string{
-	"testdata/basic":     "./testdata/expected/basic",
-	"testdata/interface": "./testdata/expected/interface",
+	"testdata/basic":     "testdata/expected/basic",
+	"testdata/interface": "testdata/expected/interface",
 }
 
 var failures []string
 
 func TestInstrumentation(t *testing.T) {
 	cwd, _ := os.Getwd()
-	_ = cwd
 	var args []string
 	for k, _ := range testcases {
 		filePaths := make(map[string]int)
 
 		files := alib.SearchFiles(k, ".go")
 		for index, file := range files {
-			fmt.Println(file)
 			filePaths[file] = index
 		}
 		pruner := rewriters.OtelPruner{ProjectPath: k,
@@ -52,4 +54,35 @@ func TestInstrumentation(t *testing.T) {
 		analyzePackage(rewriter, "main", filePaths, nil, "", args)
 
 	}
+	fmt.Println(cwd)
+
+	for k, v := range testcases {
+		files := alib.SearchFiles(cwd+"/"+k, ".go")
+		expectedFiles := alib.SearchFiles(cwd+"/"+v, ".go")
+		numOfFiles := len(expectedFiles)
+		fmt.Println("Go Files:", len(files))
+		fmt.Println("Expected Go Files:", len(expectedFiles))
+		numOfComparisons := 0
+		for _, file := range files {
+			fmt.Println(filepath.Base(file))
+			for _, expectedFile := range expectedFiles {
+				fmt.Println(filepath.Base(expectedFile))
+				if filepath.Base(file) == filepath.Base(expectedFile) {
+					f1, err1 := os.ReadFile(file)
+					require.NoError(t, err1)
+					f2, err2 := os.ReadFile(expectedFile)
+					require.NoError(t, err2)
+					if !assert.True(t, bytes.Equal(f1, f2), file) {
+						failures = append(failures, file)
+					}
+					numOfComparisons = numOfComparisons + 1
+				}
+			}
+		}
+		if numOfFiles != numOfComparisons {
+			fmt.Println("numberOfComparisons:", numOfComparisons)
+			panic("not all files were compared")
+		}
+	}
+
 }
